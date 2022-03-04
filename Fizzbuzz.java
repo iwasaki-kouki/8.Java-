cafce939a0f4251131dba8c3e31049c49e6bac5c
岩崎 <karausuyanosegare@gmail.com>
2022-03-04 14:13:24 +0900

    FizzBuzz first commt

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -1,5 +1,7 @@
 public class Main {
-    for (int i = 0; i < args.length; i++) {
+    public static void main(String[] args) {
+      for (int i = 0; i < args.length; i++) {
         System.out.println(args[i]);
       }
-}
+    }
+  }
